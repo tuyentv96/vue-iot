fdf2a2eaa88987bf5a539dfa30305ed69d9b4e45
Tran Tuyen <vat1906@gmail.com>
2019-10-22 23:09:20 +0700

    accept console

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
     <el-table :data="tableData" style="width: 100%" :row-class-name="tableRowClassName">
       <el-table-column prop="date" label="Date" width="180"></el-table-column>
       <el-table-column prop="name" label="Name" width="180"></el-table-column>
@@ -13,6 +12,12 @@
 </template>
 
 <script>
+let values = [];
+values.push({
+  date: "2016-05-03",
+  name: "Tom",
+  address: "No. 189, Grove St, Los Angeles"
+});
 export default {
   name: "HelloWorld",
   props: {
@@ -32,50 +37,12 @@ export default {
   },
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-03",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles"
-        },
-        {
-          date: "2016-05-02",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles"
-        },
-        {
-          date: "2016-05-04",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles"
-        },
-        {
-          date: "2016-05-01",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles"
-        }
-      ]
+      tableData: values
     };
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
 
 <style>
 .el-table .warning-row {
